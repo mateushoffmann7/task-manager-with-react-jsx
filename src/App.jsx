c6d917d87031f9fb328c4d4d261aeb65dc36423f
Mettel07 <mateushoffmann0707@gmail.com>
2025-10-06 19:07:38 -0300

    feat: implement task management functionality and add Title component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,51 @@
 import { useState } from 'react';
+import { Title } from './components/Title';
 
-export function App() {
-  return;
+function App() {
+  const [task, setTask] = useState(
+    JSON.parse(localStorage.getItem('task')) || [],
+  );
+
+  useEffect(() => {
+    localStorage.setItem('task', JSON.stringify(task));
+  }, [task]);
+  function onTaskClick(taskId) {
+    const newTasks = task.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTask(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTask = task.filter((task) => task.id !== taskId);
+    setTask(newTask);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTask([...task, newTask]);
+  }
+  return (
+    <div className="h-screen w-screen bg-slate-500 flex justify-center text-center p-6">
+      <div className="w-[500px] space-y-4">
+        <Title>Gerenciador de Tarefas</Title>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={task}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
+    </div>
+  );
 }
+
+export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,3 @@
+export function Title({ children }) {
+  return <h1 className="text-3xl text-slate-100 font-bold">{children}</h1>;
+}
